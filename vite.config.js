bc7802c98b53413d50703210e25a75201ad9800f
BattinaVedhaSri <vedhasribattina@gmail.com>
2026-09-24 19:33:40 +0530

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Pet_Adoption_Platform/',
+  base: '/pet-adoption-app/',
   plugins: [react()],
 })
